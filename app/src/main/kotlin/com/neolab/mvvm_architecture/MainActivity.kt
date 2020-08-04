58db34123f33ce2b1621111974cb4f9a93598948
ThuanPx <thuanpx1710@gmail.com>
2020-08-04 16:11:50 +0700

    Init project + Add ktlint

---
+++ app/src/main/kotlin/com/neolab/mvvm_architecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neolab.mvvm_architecture
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
