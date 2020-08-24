d6360114f474f2ccfa48af21b5ed17851e2c7922
hieunt142 <hieunt142@gmail.com>
2020-08-24 16:09:11 +0700

    Edit git hooks: change command base on OS family

--- team-props/git-hooks.gradle.kts
+++ team-props/git-hooks.gradle.kts
@@ -1,3 +1,4 @@
+import org.apache.tools.ant.taskdefs.condition.Os
 tasks.register("copyGitHooks", Copy::class) {
     description = "Copies the git hooks from team-props/git-hooks to the .git folder."
     val path = "$rootDir/team-props/git-hooks"
@@ -12,7 +13,11 @@ tasks.register("installGitHooks", Exec::class) {
     description = "Installs the pre-commit git hooks from team-props/git-hooks."
     group = "git hooks"
     workingDir = rootDir
-    setCommandLine("chmod", "-R", "+x", ".git/hooks/")
+    if (Os.isFamily(Os.FAMILY_WINDOWS)) {
+        setCommandLine("cmd", "/c","ver")
+    } else {
+        setCommandLine("chmod", "-R", "+x", ".git/hooks/")
+    }
     dependsOn("copyGitHooks")
     doLast {
         logger.info("Git hook installed successfully!")
