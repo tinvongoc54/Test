6df11b5b7d0737fa7c6d7e3ddede671e55bbf511
thang <tranducthang1505@gmail.com>
2020-09-01 13:44:45 +0700

    add remove key and check contains in shared preferences functions

--- app/src/main/kotlin/com/neolab/mvvm_architecture/data/local/sharedpfers/SharedPrefsWrapper.kt
+++ app/src/main/kotlin/com/neolab/mvvm_architecture/data/local/sharedpfers/SharedPrefsWrapper.kt
@@ -46,4 +46,11 @@ class SharedPrefsWrapper(val sharedPrefs: SharedPreferences, val gson: Gson) {
             else -> sharedPrefs.jsonLiveData(key, gson, T::class.java)
         } as LiveData<T>
     }
+    
+    fun removeKey(key: String, commitNow: Boolean = false) {
+        sharedPrefs.edit(commitNow) {remove(key) }
+    }
+
+    infix fun contains(key: String) = sharedPrefs.contains(key)
+
 }
